eea7b00dc742f867e822f279a82a090f8c68941d
Carlos Urquiza <webquiza@gmail.com>
2021-06-19 22:20:57 -0700

    add branching logic for months id

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,46 @@ $(document).ready(function() {
   $("#survey").submit(function(e) {
     e.preventDefault();
 
+    const hours = $("#hours").val();
 
-    
+    // Declare initial variables equal to zero 
+
+    let javascriptScore = 0;
+    let pythonScore = 0;
+    let cSharpScore = 0;
+
+    // Branching logic for hours id
+   
+    if (hours === "one") {
+      javascriptScore += 1;
+    } else if (hours === "two") {
+      pythonScore += 1;
+    } else if (hours === "three") {
+      cSharpScore += 1;
+    } 
+
+    // Branching logic for months id
+
+    if (months === "one") {
+      javascriptScore += 1;
+    } else if (months === "two") {
+      pythonScore += 1;
+    } else if (months === "three") {
+      cSharpScore += 1;
+    } 
+
+    // Branching logic that provides language suggestion to user
+
+    if (javascriptScore > pythonScore && javascriptScore > cSharpScore) {
+      $("#suggested-language").text("You should learn JavaScript");
+    } else if (pythonScore > javascriptScore && pythonScore > cSharpScore) {
+      $("#suggested-language").text("You should learn Python");
+    } else if (cSharpScore > javascriptScore && cSharpScore > pythonScore) {
+      $("#suggested-language").text("You should learn C#");
+    } else {
+      $("#suggested-language").text("Hmm... Something is not adding up here. Don't forget to select an answer for each question!");
+    }
+
     $("#output").show();
   });
 });
